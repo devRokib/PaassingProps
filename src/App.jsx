24a94c588c0e9b6a0339e1b74766c0f940e6910b
devRokib <rakibahmedra971162@gmail.com>
2023-12-28 13:25:40 +0600

    container config done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import Avatar from "./component/Avater";
  function Profile() {
     
   return (
+    <>
+    <p>hello world</p>
     <Avatar name={'jonh doe'} size={'30px'} />
+    </>
   );
 }
 export default Profile;
